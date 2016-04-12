3b7a35a4d0a8a7978ba26c87bc4555f6cb2faabb
erdidursun <erdidursun09@hotmail.com>
2016-04-12 17:10:37 +0300

    sosyal medya giriş ayarları

--- js/controllers.js
+++ js/controllers.js
@@ -3,7 +3,7 @@ sakaryarehberi
 .controller("MainCtrl", function ($scope) {
 })
 
-.controller("HeaderCtrl", function ($scope, $state, $uibModal, AUTH_EVENTS, User, AuthService) {
+.controller("HeaderCtrl", function ($scope, $state, $uibModal, User, AuthService) {
     var userInfo = User.Info();
     $scope.isLogged = userInfo?userInfo.isAuthanthanced:false;
     $scope.profileImg = userInfo && userInfo.data ? userInfo.data.profileImageURL : "../assets/layouts/layout3/img/avatar9.jpg";
@@ -37,11 +37,11 @@ sakaryarehberi
     };
 })
 
-.controller("LoginCtrl", function ($scope,$state, AuthService,$modalInstance, AUTH_EVENTS, User, $uibModal) {
+.controller("LoginCtrl", function ($scope,$state, AuthService, AUTH_EVENTS, User, $uibModal) {
 
 
     $scope.$on(AUTH_EVENTS.loginSuccess, function (data) {
-        $modalInstance.close();
+        //$modalInstance.close();
         $state.go("home", {}, { reload: true });
 
 
@@ -63,10 +63,9 @@ sakaryarehberi
 .controller("CarouselDemoCtrl",function($scope) {
     $scope.myInterval = 1000;
     var slides = $scope.slides = [];
-    $scope.slides.push({ image: "assets/global/img/600x600/1.jpg", text: "test", active: true });
-    $scope.slides.push({ image: "assets/global/img/600x600/2.jpg", text: "test2", active: false });
-    $scope.slides.push({ image: "assets/global/img/600x600/3.jpg", text: "test2", active: false });
-    $scope.slides.push({ image: "assets/global/img/600x600/4.jpg", text: "test2", active: false });
+    $scope.slides.push({ image: "assets/global/img/1.jpg", text: "test", active: true });
+    $scope.slides.push({ image: "assets/global/img/2.jpg", text: "test2", active: false });
+    $scope.slides.push({ image: "assets/global/img/3.jpg", text: "test2", active: false });
 
     $scope.slides.push({ image: "http://placekitten.com/503/300", text: "asdasdsad", active: false });
 })
--- js/sakaryarehberi.js
+++ js/sakaryarehberi.js
@@ -11,9 +11,9 @@
               url: "/",
               templateUrl: 'views/main.html',
               controller: "MainCtrl"
-          }).state('login', {
-              url: "/login",
-              templateUrl: 'views/partials/logintemplate.html',
+          }).state('home.login', {
+              url: "login",
+              templateUrl: 'views/partials/login.html',
               controller: 'LoginCtrl',
               resolve: {
                   deps: ['$ocLazyLoad', function ($ocLazyLoad) {
@@ -30,6 +30,45 @@
                   }]
               }
           })
+          .state('home.forgot', {
+              url: "forgot",
+              templateUrl: 'views/partials/forgot.html',
+              controller: 'LoginCtrl',
+              resolve: {
+                  deps: ['$ocLazyLoad', function ($ocLazyLoad) {
+                      return $ocLazyLoad.load({
+                          name: 'sakaryarehberi',
+                          insertBefore: '#ng_load_plugins_before', // load the above css files before '#ng_load_plugins_before'
+                          files: [
+                              'assets/global/css/login.min.css',
+                              "assets/global/plugins/jquery-validation/js/jquery.validate.min.js",
+                              "assets/global/plugins/jquery-validation/js/additional-methods.js",
+                              "assets/pages/scripts/login.min.js"
+                          ]
+                      });
+                  }]
+              }
+          })
+      .state('home.register', {
+          url: "register",
+          templateUrl: 'views/partials/register.html',
+          controller: 'LoginCtrl',
+          resolve: {
+              deps: ['$ocLazyLoad', function ($ocLazyLoad) {
+                  return $ocLazyLoad.load({
+                      name: 'sakaryarehberi',
+                      insertBefore: '#ng_load_plugins_before', // load the above css files before '#ng_load_plugins_before'
+                      files: [
+                          'assets/global/css/login.min.css',
+                          "assets/global/plugins/jquery-validation/js/jquery.validate.min.js",
+                          "assets/global/plugins/jquery-validation/js/additional-methods.js",
+                          "assets/pages/scripts/login.min.js"
+                      ]
+                  });
+              }]
+          }
+      })
+  
     $urlRouterProvider.otherwise("/");
 })
 .run(function ($rootScope, AUTH_EVENTS, AuthService) {
