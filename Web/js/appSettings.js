f3ec092fed9ac547b1019c17bc7228aa59bab76c
Erdi Dursun <Erdi Dursun>
2016-05-06 16:25:19 +0300

    ww

--- Web/js/appSettings.js
+++ Web/js/appSettings.js
@@ -2,7 +2,7 @@
 
     apiHostUrl: "tommycarter-001-site1.itempurl.com",
     //apiHostUrl: "localhost:8054",
-    apiHostUrl: "localhost:8090",
+    //apiHostUrl: "localhost:8090",
 
     logingEnabled:false
 }
