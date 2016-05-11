4ed28a20ef0024d6db7bc303fa025427e807fd13
erdidursun <erdidursun09@hotmail.com>
2016-05-11 16:00:47 +0300

    ss

--- Web/js/appSettings.js
+++ Web/js/appSettings.js
@@ -1,8 +1,5 @@
 var Settings = {
-    //apiHostUrl: "tommycarter-001-site1.itempurl.com",
-    apiHostUrl: "localhost:8054",
-    //apiHostUrl: "localhost:8090",
-
+   apiHostUrl: "tommycarter-001-site1.itempurl.com",
     //apiHostUrl: "tommycarter-001-site1.itempurl.com",
     //apiHostUrl: "localhost:8054",
 
