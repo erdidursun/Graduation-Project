2dd91be022b32b1cc34c3d86dc55d24d8ed60729
Ahmet Ozan Ceylan <cylnozan@gmail.com>
2016-05-16 21:15:34 +0300

    ozan

--- Web/js/appSettings.js
+++ Web/js/appSettings.js
@@ -1,13 +1,4 @@
 var Settings = {
-<<<<<<< HEAD
-    //apiHostUrl: "tommycarter-001-site1.itempurl.com",
-    apiHostUrl: "localhost:8054",
-    apiHostUrl: "localhost:8090",
-=======
-    apiHostUrl: "tommycarter-001-site1.itempurl.com",
-    //apiHostUrl: "localhost:8054",
-    //apiHostUrl: "localhost:8090",
->>>>>>> 07f62ad17e7350d10aa82b3e162fd1e191700205
 
     //apiHostUrl: "tommycarter-001-site1.itempurl.com",
     //apiHostUrl: "localhost:8054",
