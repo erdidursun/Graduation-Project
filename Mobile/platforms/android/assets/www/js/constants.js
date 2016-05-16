26a723a2e8c0a5fa883303f12f86bd7e55ce5c6b
Erdi Dursun <Erdi Dursun>
2016-05-16 21:45:29 +0300

    clean

--- Mobile/platforms/android/assets/www/js/constants.js
+++
@@ -1,8 +0,0 @@
-﻿angular.module('sakaryarehberi').constant('AUTH_EVENTS', {
-    loginSuccess: 'auth-login-success',
-    loginFailed: 'auth-login-failed',
-    logoutSuccess: 'auth-logout-success',
-    sessionTimeout: 'auth-session-timeout',
-    notAuthenticated: 'auth-not-authenticated',
-    notAuthorized: 'auth-not-authorized'
-})
