12a979479e2ce6b219c9153d3a8a0cf52cff110b
Ahmet Ozan Ceylan <cylnozan@gmail.com>
2016-05-19 00:33:31 +0300

    adminpanel

--- Web/js/appSettings.js
+++ Web/js/appSettings.js
@@ -1,8 +1,8 @@
 var Settings = {
 
-    apiHostUrl: "tommycarter-001-site1.itempurl.com",
+    //apiHostUrl: "tommycarter-001-site1.itempurl.com",
     //apiHostUrl: "localhost:8054",
-    //apiHostUrl: "localhost:8090",
+    apiHostUrl: "localhost:8090",
 
     logingEnabled:false
 
